02a293945b23d1b6ec8792770b7d74eb1b291316
Vanessa Pyne <vipyne@gmail.com>
2013-11-18 17:51:28 -0800

    all the things, second commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,33 @@
-class Categories < ApplicationController
+class CategoriesController < ApplicationController
 
   def index
-    
+    @category = Category.new
+    @categories = Category.all
   end
+
+  def create
+    p params
+    @category = Category.create(params[:category])
+    redirect_to root_path
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @posts = @category.posts
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    Category.find(params[:id]).update_attributes(params[:category])
+    redirect_to category_path        
+  end
+  
+  def destroy
+    Category.find(params[:id]).destroy
+    redirect_to category_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 CraigslistSr::Application.routes.draw do
  root :to => 'categories#index'
 
- resources :posts, only: [:new]
-
- resources :categories do
-   resources :posts, shallow: true, except: [:new]
- end
+ resources :posts
+ resources :categories
  
 end
